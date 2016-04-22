21868b572a9361bc7b43753fd57e2af07c816491
ET-Nik <nikita.hldm@gmail.com>
2016-04-23 00:42:14 +0300

    Config vars

--- config.h
+++ config.h
@@ -1,5 +1,21 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
 class Config
 {
     public:
+        std::string db_driver;
+        std::string db_host;
+        std::string db_user;
+        std::string db_passwd;
+        std::string db_name;
+        int db_port;
+
+        std::string crypt_key;
+        std::string allowed_ip_str;
+        int port;
+        
         int parse();
 };
+
+#endif
