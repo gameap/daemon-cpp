02bdc6da91bea5ce6623f4ed49d4ccce592266c0
ET-Nik <nikita.hldm@gmail.com>
2016-04-28 02:43:56 +0300

    File send

--- file_server.cpp
+++ file_server.cpp
@@ -1,15 +1,21 @@
 #include "file_server.h"
 
 #include "functions/gcrypt.h"
+#include "config.h"
 
 using boost::asio::ip::tcp;
+
+// ---------------------------------------------------------------------
 
 void FileServerSess::start ()
 {
     write_binn = binn_list();
+    mode = 1;
     aes_key = "12345678901234561234567890123456";
     do_read();
 }
+
+// ---------------------------------------------------------------------
 
 void FileServerSess::do_read()
 {
@@ -19,43 +25,153 @@ void FileServerSess::do_read()
             if (!ec) {
 
                 read_length += length;
+                // std::cout << "MODE: " << mode << std::endl;
 
-                if (strstr(read_buf, "\xFF\xFF\xFF\xFF") != nullptr) {
-                    binn_free(write_binn);
-                    write_binn = binn_list();
-                    
-                    std::string msg(read_buf, read_length-4);
-                    binn_list_add_str(write_binn, &msg[0]);
-                    
-                    read_length = 0;
-                    memset(read_buf, 0, max_length-1);
-                    
-                    do_write();
+                if (mode == 3) {
+                    // std::cout << "Write File" << std::endl;
+                    write_file(length);
+                } else {
+
+                    if (strstr(read_buf, "\xFF\xFF\xFF\xFF") != nullptr) {
+                        // std::string msg(read_buf, read_length-4);
+                        // binn_list_add_str(write_binn, &msg[0]);
+                        switch (mode) {
+                            case 0: {
+                                break;
+                            };
+                            case 1: {
+                                cmd_process();
+                                break;
+                            };
+
+                            case 2: {
+                                break;
+                            };
+                        }
+
+                    }
                 }
+
+                // read_length += length;
+
+                // if (strstr(read_buf, "\xFF\xFF\xFF\xFF") != nullptr) {
+                    // binn_free(write_binn);
+                    // write_binn = binn_list();
+
+                    // std::string msg(read_buf, read_length-4);
+                    // binn_list_add_str(write_binn, &msg[0]);
+
+                    // read_length = 0;
+                    // memset(read_buf, 0, max_length-1);
+
+                    // do_write();
+                // }
             }
     });
 }
 
 void FileServerSess::do_write()
 {
+    read_length = 0;
+    memset(read_buf, 0, max_length-1);
+
     auto self(shared_from_this());
-    char *msg;
+    char send[max_length];
 
     size_t len = 0;
-    
+
     // msg = GCrypt::aes_encrypt((char*)binn_ptr(write_binn), aes_key);
-    msg = (char*)binn_ptr(write_binn);
+    // send = (char*)binn_ptr(write_binn);
 
-    strcat(msg, "\xFF\xFF\xFF\xFF");
-    // std::cout << "SEND: " << msg << std::endl;
+    strcpy(send, (char*)binn_ptr(write_binn));
+    strcat(send, "\xFF\xFF\xFF\xFF");
+    std::cout << "SEND: " << send << std::endl;
 
-    boost::asio::async_write(socket_, boost::asio::buffer(msg, strlen(msg)),
+    boost::asio::async_write(socket_, boost::asio::buffer(send, strlen(send)),
         [this, self](boost::system::error_code ec, std::size_t) {
             if (!ec) {
                 do_read();
             }
     });
 }
+
+void FileServerSess::cmd_process()
+{
+    binn *read_binn;
+    std::string msg(read_buf, read_length-4);
+    read_binn = binn_open((void*)msg.c_str());
+
+    int command;
+    command = binn_list_int16(read_binn, 1);
+
+    switch (command) {
+        case 2: {
+            // Shell command
+        };
+
+        case 3: {
+            // File send
+            filename = binn_list_str(read_binn, 2);
+            size_t filesize = binn_list_int16(read_binn, 3);
+            int chmod = binn_list_int16(read_binn, 4);
+            bool make_dir = binn_list_bool(read_binn, 5);
+
+            // Check
+            // ...
+
+            std::cout << "filename: " << filename << std::endl;
+            std::cout << "filesize: " << filesize << std::endl;
+            std::cout << "chmod: " << chmod << std::endl;
+            std::cout << "make_dir: " << make_dir << std::endl;
+
+            binn_release(write_binn);
+            write_binn = binn_list();
+
+            // ALL OK
+            binn_list_add_int16(write_binn, 100);
+            binn_list_add_str(write_binn, "OK");
+
+            mode = 3;
+            open_file();
+
+            do_write();
+        };
+    }
+}
+
+void FileServerSess::open_file()
+{
+    output_file.open(filename, std::ios_base::binary | std::ios_base::ate);
+    if (!output_file) {
+        std::cout << "failed to open " << filename << std::endl;
+        return;
+    }
+
+    std::cout << "File opened: " << filename << std::endl;
+}
+
+void FileServerSess::write_file(size_t length)
+{
+    if (output_file.eof() == false) {
+        // std::cout << "writed bytes: " << length << std::endl;
+        output_file.write(read_buf, length);
+    } else {
+        std::cout << "CLOSE FILE" << std::endl;
+        close_file();
+    }
+
+    do_read();
+}
+
+void FileServerSess::close_file()
+{
+    output_file.close();
+    mode = 1;
+}
+
+// ---------------------------------------------------------------------
+
+// ---------------------------------------------------------------------
 
 void FileServer::do_accept()
 {
@@ -64,23 +180,25 @@ void FileServer::do_accept()
         if (!ec) {
             std::make_shared<FileServerSess>(std::move(socket_))->start();
         }
-        
+
         do_accept();
     });
 }
+
+// ---------------------------------------------------------------------
 
 int run_file_server(int port)
 {
     try {
         boost::asio::io_service io_service;
-        
+
         FileServer s(io_service, port);
-        
+
         io_service.run();
     }
     catch (std::exception& e) {
         std::cerr << "Exception: " << e.what() << "\n";
     }
-    
+
     return 0;
 }
--- file_server.h
+++ file_server.h
@@ -9,43 +9,69 @@
 #include <utility>
 #include <boost/asio.hpp>
 
+#include <fstream>
+
 #include <binn.h>
+
+// ---------------------------------------------------------------------
 
 class FileServerSess : public std::enable_shared_from_this<FileServerSess> {
 public:
-  FileServerSess(boost::asio::ip::tcp::socket socket) : socket_(std::move(socket)) {};
-  void start();
+    FileServerSess(boost::asio::ip::tcp::socket socket) : socket_(std::move(socket)) {};
+    void start();
 
 private:
-  void do_read();
-  void do_write();
+    void do_read();
+    void do_write();
 
-  boost::asio::ip::tcp::socket socket_;
-  enum { max_length = 1024 };
-  
-  std::size_t read_length;
-  char read_buf[max_length];
-  
-  binn *write_binn;
-  char *aes_key;
+    void cmd_process();
+
+    void open_file();
+    void write_file(size_t length);
+    void close_file();
+
+    boost::asio::ip::tcp::socket socket_;
+    enum { max_length = 1024 };
+    
+    std::size_t read_length;
+    char read_buf[max_length];
+    
+    binn *write_binn;
+    char *aes_key;
+
+    /*
+     * 0 - NoAuth
+     * 1 - Auth
+     * 2 - Cmd
+     * 3 - File send
+     * */
+    int mode;
+
+    boost::asio::streambuf request_buf;
+    char *filename;
+    std::ofstream output_file;
 };
+
+// ---------------------------------------------------------------------
 
 class FileServer
 {
 public:
 FileServer(boost::asio::io_service& io_service, short port)
-    : acceptor_(io_service, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port)),
-    socket_(io_service)
+        : acceptor_(io_service, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port)),
+        socket_(io_service)
 {
-    do_accept();
+        do_accept();
 };
 
 private:
-  void do_accept();
-  
-  boost::asio::ip::tcp::acceptor acceptor_;
-  boost::asio::ip::tcp::socket socket_;
+    void do_accept();
+    
+    boost::asio::ip::tcp::acceptor acceptor_;
+    boost::asio::ip::tcp::socket socket_;
 };
+
+// ---------------------------------------------------------------------
 
 int run_file_server(int port);
 
