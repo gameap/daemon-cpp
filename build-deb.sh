898917e5a070f0363af750a4c92c74f431985966
ET-Nik <nikita.hldm@gmail.com>
2016-11-17 14:35:01 +0300

    Travis build

---
+++ build-deb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apt-get -y install sudo wget
+wget -qO - http://packages.gameap.ru/debian/gameap-rep.gpg.key | sudo apt-key add -
+echo "deb http://packages.gameap.ru/debian jessie main" >> /etc/apt/sources.list
+
+apt-get -qq update
+
+apt-get -y install build-essential make cmake gcc-4.8 g++-4.8
+
+apt-get -y install libboost-system1.55-dev libboost-filesystem1.55-dev libboost-iostreams1.55-dev libboost-thread1.55-dev
+apt-get -y install libboost-process1.55-dev libbinn-dev
+apt-get -y install libjsoncpp-dev libssl-dev libmcrypt-dev
+
+cmake .
+make
