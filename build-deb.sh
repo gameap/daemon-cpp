59aaff3952c5e88a84571f8d4097212e4e8f8f94
ET-Nik <nikita.hldm@gmail.com>
2016-11-17 15:05:10 +0300

    sudo ./build-deb.sh

--- build-deb.sh
+++ build-deb.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-sudo wget -qO - http://packages.gameap.ru/debian/gameap-rep.gpg.key | sudo apt-key add -
-sudo echo "deb http://packages.gameap.ru/debian jessie main" >> /etc/apt/sources.list
+wget -qO - http://packages.gameap.ru/debian/gameap-rep.gpg.key | sudo apt-key add -
+echo "deb http://packages.gameap.ru/debian jessie main" >> /etc/apt/sources.list
 
-sudo apt-get -qq update
+apt-get -qq update
 
-sudo apt-get -y install build-essential make cmake gcc-4.8 g++-4.8
+apt-get -y install build-essential make cmake gcc-4.8 g++-4.8
 
-sudo apt-get -y install libboost-system1.55-dev libboost-filesystem1.55-dev libboost-iostreams1.55-dev libboost-thread1.55-dev
-sudo apt-get -y install libboost-process1.55-dev libbinn-dev
-sudo apt-get -y install libjsoncpp-dev libssl-dev libmcrypt-dev
+apt-get -y install libboost-system1.55-dev libboost-filesystem1.55-dev libboost-iostreams1.55-dev libboost-thread1.55-dev
+apt-get -y install libboost-process1.55-dev libbinn-dev
+apt-get -y install libjsoncpp-dev libssl-dev libmcrypt-dev
 
 cmake .
 make
