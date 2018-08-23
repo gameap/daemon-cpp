1e79daa33210592569a7619d486927bd1e1e7e29
ET-Nik <nikita.hldm@gmail.com>
2018-08-23 07:20:49 +0300

    Update build

--- build-deb.sh
+++ build-deb.sh
@@ -10,7 +10,7 @@ apt-get -y install build-essential make cmake
 apt-get -y install libboost-dev libboost-system-dev libboost-filesystem-dev libboost-iostreams-dev libboost-thread-dev
 apt-get -y install libboost-process-dev libbinn-dev
 apt-get -y install libjsoncpp-dev libssl-dev libmysqlclient-dev
-apt-get -y install libcpprest-dev
+apt-get -y install librestclientcpp-dev
 
 cmake .
 make
