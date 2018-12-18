83b111de07c1089860385f4493263e090bb5297e
et-nik <nikita.hldm@gmail.com>
2018-12-18 19:51:59 +0300

    Add linux build script. Manually compiling all packages dependencies

---
+++ build-linux.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+ROOTDIR=$(pwd)
+
+# Compile GCC 7
+# wget http://mirror.linux-ia64.org/gnu/gcc/releases/gcc-7.4.0/gcc-7.4.0.tar.gz
+# tar -xvf gcc-7.4.0.tar.gz
+# cd gcc-7.4.0
+# ./contrib/download_prerequisites
+# mkdir objdir && cd objdir
+# $PWD/../configure --enable-multilib --disable-shared --with-newlib
+# make
+
+# Download CMake
+wget https://cmake.org/files/v3.10/cmake-3.10.3-Linux-x86_64.tar.gz
+tar -xvf cmake-3.10.3-Linux-x86_64.tar.gz
+cp -R cmake-3.10.3-Linux-x86_64/* /
+
+# Compile Boost
+cd $ROOTDIR
+wget http://packages.gameap.ru/others/boost_1_65_1.tar.gz
+tar -xvf boost_1_65_1.tar.gz
+cd boost_1_65_1
+./bootstrap.sh
+./b2 install --link=static
+
+# Compile Binn
+cd $ROOTDIR
+git clone https://github.com/liteserver/binn
+cd binn
+make
+ar rcs libbinn.a binn.o
+cp libbinn.a /usr/lib/libbinn.a
+cp src/binn.h /usr/include/binn.h
+
+# OpenSSL
+cd $ROOTDIR
+wget https://github.com/openssl/openssl/archive/OpenSSL_1_1_1a.tar.gz
+tar -xvf OpenSSL_1_1_1a.tar.gz
+cd openssl-OpenSSL_1_1_1a
+./config
+make
+make install
+
+# Compile Curl Library
+cd $ROOTDIR
+wget https://curl.haxx.se/download/curl-7.63.0.tar.gz
+cd curl-7.63.0
+cmake -DCMAKE_USE_OPENSSL=on .
+make
+make install
+
+# Compile Rest Client Library
+cd $ROOTDIR
+git clone https://github.com/mrtazz/restclient-cpp
+cd restclient-cpp
+./autogen.sh
+./configure
+make
+make install
+
+# Compile Json Cpp
+cd $ROOTDIR
+git clone https://github.com/open-source-parsers/jsoncpp
+cd jsoncpp
+cmake .
+make
+make install
+mkdir /usr/local/include/jsoncpp
+mv /usr/local/include/json /usr/local/include/jsoncpp/json
+
+# Compile GDAEMON
+#--------------------------
+cd $ROOTDIR
+git clone https://github.com/gameap/GDaemon2
+cd GDaemon2
+cmake -DUSE_STATIC_BOOST=1 .
+make
