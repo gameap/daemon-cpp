bf0f2dd3995bedf4c14a874069b602e934f0c922
et-nik <nikita.hldm@gmail.com>
2018-12-18 18:59:36 +0300

    Add Ubuntu build scripts: trusty, xenial, bionic, cosmic

---
+++ build-ubuntu-cosmic.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+apt-get -y -qq update
+apt-get -y install curl software-properties-common
+
+curl -fsSL http://packages.gameap.ru/gameap-rep.gpg.key | apt-key add -
+echo "deb http://packages.gameap.ru/ubuntu cosmic main" > /etc/apt/sources.list.d/gameap.list
+add-apt-repository -y ppa:mhier/libboost-latest
+
+apt-get -y -qq update
+
+apt-get -y install build-essential make cmake pkg-config
+
+apt-get -y install \
+    libboost-system-dev \
+    libboost-filesystem-dev \
+    libboost-iostreams-dev \
+    libboost-thread-dev \
+    libbinn-dev \
+    librestclientcpp-dev \
+    libjsoncpp-dev \
+    libssl-dev \
+    libcurl4-openssl-dev
+
+cmake .
+make
