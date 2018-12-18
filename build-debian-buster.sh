e0e27008042213a494bd2366c39960de8fccfc2f
et-nik <nikita.hldm@gmail.com>
2018-12-18 19:50:30 +0300

    Add Debian build scripts: jessie, stretch, buster, sid

---
+++ build-debian-buster.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+apt-get -y -qq update
+apt-get -y install curl gnupg2 software-properties-common
+
+curl -fsSL http://packages.gameap.ru/gameap-rep.gpg.key | apt-key add -
+echo "deb http://packages.gameap.ru/debian buster main" > /etc/apt/sources.list.d/gameap.list
+
+apt-get -y -qq update
+
+apt-get -y install build-essential make cmake pkg-config
+
+apt-get -y install \
+    libboost-dev \
+    libboost-system-dev \
+    libboost-filesystem-dev \
+    libboost-iostreams-dev \
+    libboost-thread-dev \
+    libbinn-dev \
+    librestclientcpp-dev \
+    libjsoncpp-dev \
+    libssl-dev \
+    libcurl4-openssl-dev
+
+cmake .
+make
--- build-debian-jessie.sh
+++ build-debian-jessie.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 apt-get -y -qq update
-apt-get -y install curl
+apt-get -y install curl gnupg2
 
-curl -fsSL http://packages.gameap.ru/debian/gameap-rep.gpg.key | apt-key add -
+curl -fsSL http://packages.gameap.ru/gameap-rep.gpg.key | apt-key add -
 echo "deb http://packages.gameap.ru/debian jessie main" >> /etc/apt/sources.list
 
 apt-get -qq update
 
-apt-get -y install build-essential make cmake
+apt-get -y install build-essential make cmake pkg-config
+apt-get -y install libbinn-dev librestclientcpp-dev libjsoncpp-dev libssl-dev libcurl4-openssl-dev
 
-apt-get -y install libboost-dev libboost-system-dev libboost-filesystem-dev libboost-iostreams-dev libboost-thread-dev libboost-process-dev
-apt-get -y --allow-unauthenticated install libbinn-dev libjsoncpp-dev libssl-dev
-apt-get -y install librestclientcpp-dev
+# Compile Boost
+wget http://packages.gameap.ru/others/boost_1_65_1.tar.gz
+tar -xvf boost_1_65_1.tar.gz
+cd boost_1_65_1
+./bootstrap.sh
+./b2 install --link=static
 
 cmake .
 make
