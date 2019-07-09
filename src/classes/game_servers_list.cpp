c701552af94ec32cd81f0ff429e8f3043168b761
et-nik <nikita.hldm@gmail.com>
2019-07-09 19:31:57 +0300

    Update vars before insert

--- src/classes/game_servers_list.cpp
+++ src/classes/game_servers_list.cpp
@@ -34,6 +34,8 @@ int GameServersList::update_list()
             std::shared_ptr<GameServer> gserver = std::make_shared<GameServer>(server_id);
 
             try {
+                gserver->update(true);
+
                 servers_list.insert(
                         servers_list.end(),
                         std::pair<ulong, std::shared_ptr<GameServer>>(server_id, gserver)
